679ed2135247c60f1bf80679592f58833cba2919
Adam Wrona <adamwronowy@gmail.com>
2025-11-21 11:05:42 +0800

    Infra: add placeholder outputs for state-backend module

---
+++ infra/terraform/modules/state-backend/outputs.tf
@@ -0,0 +1,13 @@
+###############################################################################
+# Outputs (currently placeholders, will be filled once resources are added)
+###############################################################################
+
+# output "state_bucket_name" {
+#   value       = aws_s3_bucket.tf_state.bucket
+#   description = "Name of the S3 bucket used for Terraform remote state."
+# }
+#
+# output "lock_table_name" {
+#   value       = aws_dynamodb_table.tf_locks.name
+#   description = "Name of the DynamoDB table used for state locking."
+# }
